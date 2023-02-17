3df213c06c9b5e7de5c3ded217cb536528600682
RAGHURAM SADINENI <sadineni.raghuram@gmail.com>
2023-02-16 18:31:48 -0600

    i think max intro page is complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()]
+  integrations: [tailwind(), image()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		screens: {
+			'xs': '0px',
+			'sm': '576px',
+			'md': '768px',
+			'lg': '992px',
+			'xl': '1280px',
+			'2xl': '1536px'
+		  },
 	},
 	plugins: [],
 	darkMode: "class"
