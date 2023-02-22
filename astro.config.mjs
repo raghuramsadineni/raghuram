32cdc71ef11315119c13a2264178b90e897cb560
RAGHURAM SADINENI <sadineni.raghuram@gmail.com>
2023-02-21 23:39:02 -0600

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image()],
+  adapter: vercel()
 });
