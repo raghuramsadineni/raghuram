01e1a73fe3d1a5ecd13850a147eb76621a532ab6
RAGHURAM SADINENI <sadineni.raghuram@gmail.com>
2023-03-06 10:38:01 -0600

    trying to add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,14 +11,29 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 import robotsTxt from "astro-robots-txt";
+import { VitePWA } from "vite-plugin-pwa";
+import { manifest, seoConfig } from "./src/seoConfig";
+import compress from "astro-compress";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: seoConfig.baseURL,
   output: 'server',
   integrations: [tailwind(), image(), robotsTxt({
-    policy:[
-      {userAgent: '*', disallow: ''}
-    ]
+    policy: [{
+      userAgent: '*',
+      disallow: ''
+    }]
+  }), sitemap(), compress()],
+  plugins: [VitePWA({
+    registerType: "autoUpdate",
+    manifest,
+    devOptions:{
+      enabled: true
+    }
   })],
   adapter: vercel()
 });
---
+++ src/seoConfig.ts
@@ -0,0 +1,44 @@
+import type { ManifestOptions } from "vite-plugin-pwa";
+
+export const seoConfig = {
+	baseURL: "https://raghuramsadineni.co",
+	description:
+		"Raghuram Sadineni: Personal Portfolio website",
+	type: "website",
+	image: {
+		url: "/favicon.sv",
+		alt: "Bolt@9199",
+		width: 1200,
+		height: 630
+	},
+	siteName: "Raghuram Sadineni", 
+
+}
+
+
+export const manifest: Partial<ManifestOptions> = {
+	name: "Raghuram Sadineni",
+	short_name: "Raghuram Sadineni",
+	description: "Raghuram Sadineni: Personal Portfolio website",
+	theme_color: "##9F1239", 
+	background_color: "#ffffff",
+	display: "minimal-ui",
+	icons: [
+		{
+			src: "/icons/favicon-192x192.png",
+			sizes: "192x192",
+			type: "image/png"
+		},
+		{
+			src: "/icons/favicon-512x512.png",
+			sizes: "512x512",
+			type: "image/png"
+		},
+		{
+			src: "/favicon.svg",
+			sizes: "512x512",
+			type: "image/svg",
+			purpose: "any maskable"
+		}
+	]
+}
